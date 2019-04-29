6f653ac5d6c1d97a66b74f04f0a95192468496cd
Fernando Silva <development.ferjssilva@gmail.com>
2019-04-29 17:11:46 -0300

    feature: add router and page not found

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+// App components
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Header from './layout/Header';
+
 
 function App() {
   return (
-    <div className="container">
-      <Home />
-    </div>
+    <Router>
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
